f876517ed816e7ce9e949a4782c3f4a4ac92192d
mlamont8 <mlamont8@hotmail.com>
2018-01-05 21:57:47 -0500

    aligned form

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -2,11 +2,6 @@ import React from 'react';
 import SearchContainer from '../SearchContainer/SearchContainer';
 import CurrentContainer from '../CurrentContainer/CurrentContainer';
 import ForecastContainer from '../ForecastContainer/ForecastContainer';
-// import { Navbar, FormGroup, Button } from 'react-bootstrap';
-// import Autocomplete from 'react-autocomplete';
-// import jsonp from 'jsonp';
-
-
 
 
 
@@ -17,12 +12,14 @@ class Main extends React.Component {
     super(props);
     this.state = {
       retrieving: true,
+      lat: 0,
+      long: 0
     }
     this.onSearch = this.onSearch.bind(this);
   };
 
 onSearch(lat, long){
-  console.log("search update", lat, long);
+// Updated from search container when a new city is selected
   this.setState({
     lat: lat,
     long: long
@@ -57,59 +54,6 @@ onSearch(lat, long){
 
   }
 
-// retrieveData(searchText){
-//   let _this = this;
-//   jsonp('https://autocomplete.wunderground.com/aq?query='+ searchText + '&c=US&param=cb' , { param: 'cb' }, function (err, data) {
-//   if (err) {
-//     console.error("Cannot Load Data from Remote Source");
-//   } else {
-//     console.log(data.RESULTS);
-//     _this.setState({
-//       autocompleteData: data.RESULTS
-//     })
-//   }
-// });
-// }
-//
-// onChange(e){
-//     this.setState({
-//         value: e.target.value
-//     });
-//
-//     /**
-//      * Handle the remote request with the current text !
-//      */
-//     this.retrieveData(e.target.value);
-//
-//     console.log("The Input Text has changed to ", e.target.value);
-// }
-//
-// // Gets the lat and long when item is selected
-// onSelect(val,item){
-//   console.log(item);
-//     this.setState({
-//         lat: item.lat,
-//         long: item.lon
-//     });
-//
-//     console.log("Option selected : ", val);
-// }
-//
-// renderItem(item, isHighlighted){
-//     return (
-//         <div className="itemResult" style={{ background: isHighlighted ? 'lightgray' : 'black' }}>
-//             {item.name}
-//         </div>
-//     );
-// }
-//
-// getItemValue(item){
-//     // You can obviously only return the Label or the component you need to show
-//     // In this case we are going to show the value and the label that shows in the input
-//     // something like "1 - Microsoft"
-//
-//     return `${item.name}`;
-// }
 
 
 
--- src/containers/SearchContainer/SearchContainer.js
+++ src/containers/SearchContainer/SearchContainer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navbar, FormGroup, Button } from 'react-bootstrap';
+import { Navbar, FormGroup } from 'react-bootstrap';
 import Autocomplete from 'react-autocomplete';
 import jsonp from 'jsonp';
 
@@ -7,102 +7,97 @@ import jsonp from 'jsonp';
 class SearchContainer extends React.Component {
 
 
-    constructor(props) {
-      super(props);
-      this.state = {
-        cityState: '',
-        retrieving: true,
-        lat: 0,
-        long: 0,
-        value: '',
-        autocompleteData: []
-      }
-      this.onChange = this.onChange.bind(this);
-      this.onSelect = this.onSelect.bind(this);
-      this.getItemValue = this.getItemValue.bind(this);
-      this.renderItem = this.renderItem.bind(this);
-      this.retrieveData = this.retrieveData.bind(this);
-    };
+  constructor(props) {
+    super(props);
+    this.state = {
+      retrieving: true,
+      value: '',
+      autocompleteData: []
+    }
+    this.onChange = this.onChange.bind(this);
+    this.onSelect = this.onSelect.bind(this);
+    this.getItemValue = this.getItemValue.bind(this);
+    this.renderItem = this.renderItem.bind(this);
+    this.retrieveData = this.retrieveData.bind(this);
+  };
 
-  retrieveData(searchText){
+  retrieveData(searchText) {
     // Retrieve data for autocomplete.  Only if
     //searchterm is greater than 2
     let _this = this;
     if (searchText.length > 2) {
-    jsonp('https://autocomplete.wunderground.com/aq?query='+ searchText + '&c=US&param=cb' , { param: 'cb' }, function (err, data) {
-    if (err) {
-      console.error("Cannot Load Data from Remote Source");
-    } else {
-      console.log(data.RESULTS);
-      _this.setState({
-        autocompleteData: data.RESULTS
-      })
-    }
-  });
-  }
-  }
-
-  onChange(e){
-      this.setState({
-          value: e.target.value
+      jsonp('https://autocomplete.wunderground.com/aq?query=' + searchText + '&c=US&param=cb', { param: 'cb' }, function (err, data) {
+        if (err) {
+          console.error("Cannot Load Data from Remote Source");
+        } else {
+          console.log(data.RESULTS);
+          _this.setState({
+            autocompleteData: data.RESULTS
+          })
+        }
       });
-      this.retrieveData(e.target.value);
-      console.log("The Input Text has changed to ", e.target.value);
+    }
+  }
+
+  onChange(e) {
+    this.setState({
+      value: e.target.value
+    });
+    this.retrieveData(e.target.value);
   }
 
 
-  onSelect(val,item){
+  onSelect(val, item) {
     //When an item is selected, sets the new
     //lat and longitute
     console.log(item);
-      this.props.onSearch(item.lat, item.lon);
-      console.log("Option selected : ", val);
+    this.props.onSearch(item.lat, item.lon);
+    this.setState({
+      value: item.name
+    })
   }
 
-  renderItem(item, isHighlighted){
-    console.log("item", item);
-      return (
-          <div className="itemResult" style={{ background: isHighlighted ? 'lightgray' : 'black' }}>
-              {item.name}
-          </div>
-      );
+  renderItem(item, isHighlighted) {
+   
+    return (
+      <div className="itemResult" style={{ background: isHighlighted ? 'lightgray' : 'black' }}>
+        {item.name}
+      </div>
+    );
   }
 
-  getItemValue(item){
-      // You can obviously only return the Label or the component you need to show
-      // In this case we are going to show the value and the label that shows in the input
-      // something like "1 - Microsoft"
-      // this.setState({
-      //   lat: item.lat,
-      //   long: item.lon
-      // })
-      return `${item.name}`;
+  getItemValue(item) {
+// Shows Item names in the form
+
+    return `${item.name}`;
   }
 
 
   render() {
-    return(
+    return (
 
       <Navbar>
         <Navbar.Header>
           <Navbar.Brand>
             <a href="#">Better Weather</a>
           </Navbar.Brand>
+          <Navbar.Toggle />
         </Navbar.Header>
-        <Navbar.Form pullRight>
-        <FormGroup>
-          <Autocomplete
-              getItemValue={this.getItemValue}
-              items={this.state.autocompleteData}
-              renderItem={this.renderItem}
-              value={this.state.value}
-              onChange={this.onChange}
-              onSelect={this.onSelect}
-              menuStyle={{zIndex: '1', position: 'absolute'}}
-          />
-        </FormGroup>{' '}
-        <Button type='submit'>Submit</Button>
-    </Navbar.Form>
+        <Navbar.Collapse>
+          <Navbar.Form pullRight>
+            <FormGroup>
+              <Autocomplete
+                getItemValue={this.getItemValue}
+                items={this.state.autocompleteData}
+                renderItem={this.renderItem}
+                value={this.state.value}
+                onChange={this.onChange}
+                onSelect={this.onSelect}
+                menuStyle={{ zIndex: '1', position: 'absolute' }}
+              />
+            </FormGroup>{' '}
+          </Navbar.Form>
+        </Navbar.Collapse>
 
         <div>
 
@@ -110,7 +105,7 @@ class SearchContainer extends React.Component {
         </div>
       </Navbar>
 
-  )
+    )
   }
 }
 
